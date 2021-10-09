6e89e4faac9216bde9ecbc2c12dd391dcf704fbf
۩๖ۣۜℜLƤ27 <85769960+Laksmana27@users.noreply.github.com>
2021-10-09 10:51:58 +0700

    Update joke.js

--- plugins/joke.js
+++ plugins/joke.js
@@ -1,45 +1,9 @@
-// Tolong jangan bully saya bang,, saya cuman mau bisnis :)
-
-let uploadImage = require('../lib/uploadImage')
-let fetch = require('node-fetch')
-let util = require('util')
-
-let handler = async(m, { conn, args }) => {
-try {
-gay = `sayang`
-
-  if (!m.quoted) throw `Tag Fotonya ` + gay // org gay
-
-    await m.reply(`*[❗] Wait,Sedang Dalam Proses...*`)
-  let q = m.quoted ? m.quoted : m
-  let media = await q.download()
-  let img = await uploadImage(media)
-  let buffer = await (await fetch('https://leyscoders-api.herokuapp.com/api/img/joke?url=' + img + '&apikey=MIMINGANZ')).buffer() // Masukan ApiKeymu disini..
-  let res = await fetch('https://raw.githubusercontent.com/herokuapp-com/kuhong-api/main/info/credit.json')
-  let json = await res.json()
-
- await conn.sendFile(m.chat, buffer, 'joke1.jpg', 'Nih Kak', m)
-
-} catch (e) {
-
- err = await (await fetch('https://raw.githubusercontent.com/herokuapp-com/kuhong-api/main/info/erorr.json')).json() // jika Erorr
-   throw err
-  }
+let handler = async(m, { conn }) => {
+conn.sendFile(m.chat, global.API('xteam', '/asupan/darkjoke', {}, 'APIKEY'), '', 'Gelap Woy Siapa Yang Matiin Lampu?',m)
 }
-handler.help = ['joke (reply)']
-handler.tags = ['sticker']
-handler.command = /^joke$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = true
+handler.help = ['darkjokes']
+handler.tags = ['image']
+handler.command = /^(dragjoles|darkjokes)$/i
 
 module.exports = handler
+
