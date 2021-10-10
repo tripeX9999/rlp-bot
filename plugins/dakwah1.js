4e7cdea484c6fe591b75fb3980f0eb54a96e03ac
۩๖ۣۜℜLƤ27 <85769960+Laksmana27@users.noreply.github.com>
2021-10-11 04:06:13 +0700

    Update dakwah1.js

--- plugins/dakwah1.js
+++ plugins/dakwah1.js
@@ -2,12 +2,12 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './src/Audio/dakwah1.mp3'
-conn.sendFile(m.chat, vn, 'dakwah1.mp3', null, m, true, {
+let vn = './src/Audio/dakwah1.m4a'
+conn.sendFile(m.chat, vn, 'dakwah1.m4a', null, m, true, {
 type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /(Sedih|Robigfirli|Dosa sakit hati)/
+handler.customPrefix = /(Sedih|Robigfirli|Dosasakithati)/
 handler.command = new RegExp
 module.exports = handler
