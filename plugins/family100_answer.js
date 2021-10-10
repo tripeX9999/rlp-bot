49c0cdda1ad56a9058f9885f790615fe5f381f34
۩๖ۣۜℜLƤ27 <85769960+Laksmana27@users.noreply.github.com>
2021-10-11 06:32:17 +0700

    Update family100_answer.js

--- plugins/family100_answer.js
+++ plugins/family100_answer.js
@@ -9,13 +9,13 @@ module.exports = {
         let text = m.text.toLowerCase().replace(/[^\w\s\-]+/, '')
         let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
         if (!isSurrender) {
-            let index = room.jawaban.indexOf(text)
+            let index = room.jawaban.findIndex(v => v.toLowerCase().replace(/[^\w\s\-]+/, '') === text)
             if (index < 0) {
                 if (Math.max(...room.jawaban.filter((_, index) => !room.terjawab[index]).map(jawaban => similarity(jawaban, text))) >= threshold) m.reply('Dikit lagi!')
                 return !0
             }
             if (room.terjawab[index]) return !0
-            let users = global.DATABASE.data.users[m.sender]
+            let users = global.db.data.users[m.sender]
             room.terjawab[index] = m.sender
             users.exp += room.winScore
         }
@@ -26,21 +26,24 @@ module.exports = {
 Terdapat *${room.jawaban.length}* jawaban${room.jawaban.find(v => v.includes(' ')) ? `
 (beberapa jawaban terdapat spasi)
 `: ''}
-${isWin ? `*SEMUA JAWABAN TERJAWAB*`: isSurrender ? '*MENYERAH!*' : ''}
+${isWin ? `*SEMUA JAWABAN TERJAWAB*` : isSurrender ? '*MENYERAH!*' : ''}
 ${Array.from(room.jawaban, (jawaban, index) => {
             return isSurrender || room.terjawab[index] ? `(${index + 1}) ${jawaban} ${room.terjawab[index] ? '@' + room.terjawab[index].split('@')[0] : ''}`.trim() : false
         }).filter(v => v).join('\n')}
 
 ${isSurrender ? '' : `+${room.winScore} XP tiap jawaban benar`}
     `.trim()
-        m.reply(caption, null, {
+        await this.sendButton(m.chat, caption, '© Laksmana27', `${isWin ? 'Family 100' : isSurrender ? 'Family 100' : 'Nyerah'}`, `${isWin ? ',family100' : isSurrender ? ',family100' : 'nyerah'}`, m, {
             contextInfo: {
                 mentionedJid: this.parseMention(caption)
             }
         }).then(msg => {
             return this.game[id].msg = msg
-        })
+        }).catch(_ => _)
         if (isWin || isSurrender) delete this.game[id]
         return !0
     }
 }
+
+
+
