ab2a151c84eea88df942e3d0cb7d3e93e1276ec7
۩๖ۣۜℜLƤ27 <85769960+Laksmana27@users.noreply.github.com>
2021-10-11 20:19:11 +0700

    Update quotesislam.js

--- plugins/quotesislam.js
+++ plugins/quotesislam.js
@@ -7,7 +7,7 @@ json = await res.json()
 
 if (!json.result) throw 'EROR NGAB!'
 
-await conn.sendButton(m.chat, `${json.result.text_id}` ,author, 'Quotes Islamic', `${usedPrefix}quotesislam`, m)
+await conn.sendButton(m.chat, `${json.result.text_id}` ,author, 'Quotes Islamic', `${usedPrefix}quotesislam 'Doa Patah Hati', `Doa patah hati`, m)
 }
 handler.help = ['Quotesislami']
 handler.tags = ['quran']
