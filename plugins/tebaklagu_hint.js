6efc4345617e20b1705811a9ec141747dec24b9e
۩๖ۣۜℜLƤ27 <85769960+Laksmana27@users.noreply.github.com>
2021-10-11 14:46:54 +0700

    Perbaikan eror:v

--- plugins/tebaklagu_hint.js
+++ plugins/tebaklagu_hint.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.tebaklagu)) throw false
     let json = conn.tebaklagu[id][1]
-    let clue = json.judul.replace(/[AIUEOaiueo]/g, '_')
-    conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini atau audionya', conn.tebaklagu[id][0])
+    let nya = json.judul
+    let nyanya = nya.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_')
+    m.reply('```' + nyanya + '```')
 }
 handler.command = /^cek$/i
 handler.limit = true
 module.exports = handler
-    
