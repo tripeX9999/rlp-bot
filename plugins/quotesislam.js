28a952da14a597243a747cd43204fedb39e4937f
۩๖ۣۜℜLƤ27 <85769960+Laksmana27@users.noreply.github.com>
2021-10-11 20:57:07 +0700

    Dahlah..

--- plugins/quotesislam.js
+++ plugins/quotesislam.js
@@ -7,7 +7,7 @@ json = await res.json()
 
 if (!json.result) throw 'EROR NGAB!'
 
-await this.send2Button(m.chat, `${json.result.text_id}` ,author, 'Quotes Islamic', `${usedPrefix}quotesislam','Lu Sedih?', `${usedPrefix}dakwah2' m)
+await conn.sendButton(m.chat, `${json.result.text_id}` ,author, 'Quotes Islamic', `${usedPrefix}quotesislam`, m)
 }
 }
 handler.help = ['Quotesislami']
