e01d2f90c897bed42d0af87e40321be06792788f
tripeX9999 <89917062+tripeX9999@users.noreply.github.com>
2021-10-19 01:28:56 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '62895372305081', '۩๖ۣۜℜLƤ27', m)
+  this.sendContact(m.chat, '6282157541481', '©REYHAN/HANZ', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
