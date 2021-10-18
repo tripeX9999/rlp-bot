9f6ca86bb7650728edc3475998964566cbe43401
tripeX9999 <89917062+tripeX9999@users.noreply.github.com>
2021-10-19 01:19:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -34,8 +34,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'That smile is painful '
-global.author = 'RLP27'
+global.packname ='Semoga Bahagia^^'
+global.author = 'RLP27/©REYHAN'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
 global.multiplier = 39 // The higher, The harder levelup
